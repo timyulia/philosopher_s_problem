e3c7eb4eb994291414c969c490df3d02854871f5
timyulia <timoshenkojulie@icloud.com>
2022-04-25 08:15:55 +0300

    whithout host

--- main.go
+++ main.go
@@ -13,64 +13,36 @@ type philo struct {
 	rightChop *chopstick
 }
 
-func host(permission, completion chan bool) {
-	num := 0
-	for {
-		if num == 0 {
-			permission <- true
-			num++
-		}
-		if num == 1 {
-			select {
-			case permission <- true:
-				num++
-			case <-completion:
-				num--
-			}
-		}
-
-		if num == 2 {
-			<-completion
-			num--
-		}
-
-	}
-}
-
-func eat(ph *philo, ind int, wg *sync.WaitGroup, completion, permission chan bool) {
-	for i := 0; i < 3; i++ {
-		<-permission
+func eat(ph *philo, ind int, wg *sync.WaitGroup) {
+	for i := 0; i < 10; i++ {
 		ph.rightChop.Lock()
 		ph.leftChop.Lock()
 		fmt.Printf("starting to eat %d \n", ind+1)
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 		fmt.Printf("finishing eating %d \n", ind+1)
 		ph.rightChop.Unlock()
 		ph.leftChop.Unlock()
-		completion <- true
 	}
 	wg.Done()
 
 }
 
 func main() {
-	completion := make(chan bool)
-	permission := make(chan bool)
 	table := make([]*philo, 5)
 	sticks := make([]*chopstick, 5)
 	for i := 0; i < 5; i++ {
 		sticks[i] = new(chopstick)
 	}
-	for i := 0; i < 5; i++ {
+	for i := 1; i < 5; i++ {
 		table[i] = &philo{sticks[i], sticks[(i+1)%5]}
 	}
+	table[0] = &philo{sticks[1], sticks[0]}
 
 	wg := sync.WaitGroup{}
 	wg.Add(5)
 
 	for j := 0; j < 5; j++ {
-		go host(permission, completion)
-		go eat(table[j], j, &wg, completion, permission)
+		go eat(table[j], j, &wg)
 	}
 
 	wg.Wait()
